eb57eac8df067f7999527b9b1923593978e46459
cyberplanner <saleh.gfx@gmail.com>
2017-10-22 19:10:48 +0100

    Custom modules

--- README.md
+++ README.md
@@ -22,3 +22,37 @@ module.exports  = function() {
 var module = require("./module.js");
 console.log(module());
 ```
+
+Create and export a module:
+
+```js
+  // in fruit.js
+  var name = "Apple",
+    description = "Fruit from the apple tree";
+    
+  module.exports = function() {
+    var functions = {
+      setName: function(name) {
+        this.name = name;
+      },
+      setDescription: function(desc) {
+        this.description = desc;
+      },
+      getInfo: function() {
+        return {
+          name: name,
+          description: description
+        };
+      }
+    };
+    return functions;
+  }
+```
+
+Require and use it:
+
+```js
+var apple = require('./fruit');
+
+console.log(apple().getInfo());
+```
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 var sample_module = require('./sample_module');
 var class_module = require('./class_module');
 
-console.log(sample_module.randNum());
-console.log(sample_module.MagicNum);
-console.log(class_module());
+// console.log(sample_module.randNum());
+// console.log(sample_module.MagicNum);
+// console.log(class_module());
+// 
+// console.log("_________");
+// console.log("Core Modules");
+// console.log("_________");
 
-console.log("_________");
-console.log("Core Modules");
-console.log("_________");
-
-var os = require('os');
-console.log(os.platform());
-
-var http = require('http');
+// var os = require('os');
+// console.log(os.platform());
+// 
+// var http = require('http');
 // console.log(http);
+// console.log("_________");
+// var _ = require('underscore');
+
+// const names = ["Sal", "Edda", "Enya", "Randa", "Esra", "Malaka"];
+// 
+// _.each(names, function(name) {
+//   console.log(name);
+// });
+
 console.log("_________");
-var _ = require('underscore');
+console.log("Building custom modules");
+console.log("_________");
 
-const names = ["Sal", "Edda", "Enya", "Randa", "Esra", "Malaka"];
+var apple = require('./fruit');
 
-_.each(names, function(name) {
-  console.log(name);
-});
+console.log(apple().getInfo());
+
+
+var banana = require('./fruit')();
+
+banana.setName('Banana');
+console.log(banana.getInfo()); // => { name: 'Apple', description: 'Fruit from the apple tree' }
+// The fruit module will return the same apple object because it was cached before
